b8024510a8f3ae0af9b5c458321643727ea20ce8
benjtap <bntapiero@gmail.com>
2024-05-01 10:11:36 +0300

    projectsagi

--- projectAuth/screens/Logout/index.js
+++ projectAuth/screens/Logout/index.js
@@ -1,26 +1,59 @@
 import React, {useContext, useEffect} from 'react';
-import {ActivityIndicator} from 'react-native';
+import {ActivityIndicator,RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text} from 'react-native';
 import logoutUser from '../../context/actions/logoutUser';
 import {GlobalContext} from '../../context/Provider';
-//import {LOGIN} from '../../constants/routeNames';
+
 import {navigate} from '../../navigations/SideMenu/RootNavigator';
 
 const Logout = (props) => {
   const { navigation } = props;
   const {authDispatch} = useContext(GlobalContext);
+ 
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
 
   useEffect(() => {
     logoutUser()(authDispatch);
-  //  try {
-      navigate('LOGIN')
-  //  } catch (error) {
-    
-  //  }
+  
    
    
   }, []);
 
-  return <ActivityIndicator />;
-};
+  return (
+    <SafeAreaView style={styles.container}>
+    <ScrollView
+      contentContainerStyle={styles.scrollView}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      <Text>Pull down to see RefreshControl indicator</Text>
+    </ScrollView>
+  </SafeAreaView>
 
+
+  );
+};
+//<ActivityIndicator />
 export default Logout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
